1c9667499124b26177ddbdccd9468e389f91d3eb
steppbol <boloshenko.s@gmail.com>
2019-03-12 22:08:53 +0300

    refactor: add log lombok

--- Back-end/e-ground/catalog/src/main/java/com/bsuir/sdtt/CatalogApplication.java
+++ Back-end/e-ground/catalog/src/main/java/com/bsuir/sdtt/CatalogApplication.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  *
  * @author Stsiapan Balashenka
  * @version 1.0
- * @since 18-12-2018
+ * @since 20-02-2019
  */
 @SpringBootApplication
 @EnableJpaRepositories
